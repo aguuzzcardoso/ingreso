911b0ec465959003b73c08c8df03c3ed441e0de0
aguuzzcardoso <41088566+aguuzzcardoso@users.noreply.github.com>
2018-08-03 16:18:56 -0300

    Add files via upload

--- CursoIngresoJS-master/3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ CursoIngresoJS-master/3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,8 +2,29 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
-
-
+switch (mesDelAño)
+{
+	case "Enero":
+		alert("que comiences bien el año!!!.");
+		break;
+	
+	case "Febrero":
+	case "Marzo":
+		alert("a clases!!!.");
+		break;		
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+	case "Julio":
+		alert("se vienen las vacaciones!!!.")
+		break;
+	case "Agosto":
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+		alert("Felices fiesta!!!.")
+		break;	
+}
 
 }//FIN DE LA FUNCIÓN
--- CursoIngresoJS-master/3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ CursoIngresoJS-master/3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,8 +4,22 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
+switch (mesDelAño)
+{
+	case "Abril":
+	case "Junio":
+	case "Septiembre":
+	case "Noviembre":
+		alert ("30 días")
+		break;
+	case "Febrero":
+		alert ("28 días")
+		break;
+	default:
+		alert ("31 días")
+		break;
+
+}
 
 
 
